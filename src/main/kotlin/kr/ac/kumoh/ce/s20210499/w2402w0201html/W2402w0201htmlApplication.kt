288674a876026fc718bdfe67b04031e3a682f057
DESKTOP-67IRD67\박준형 <pjh6395673066@gmail.com>
2024-09-11 15:48:06 +0900

    🎉 HTML 시작

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20210499/w2402w0201html/W2402w0201htmlApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20210499.w2402w0201html
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W2402w0201htmlApplication
+
+fun main(args: Array<String>) {
+	runApplication<W2402w0201htmlApplication>(*args)
+}
